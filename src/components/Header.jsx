cbf282349c9da89c2e81afcb6de9bc3be6424372
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-04-10 16:17:36 +0300

    12l

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (<header className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfoCuO-KYuaQsPqEYUx8zYvMcswYLYqZZGyd_Usx3_eQyomUM7XTyBmFGfcw56Hu_nfEA&usqp=CAU" />
+    </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+            <main className="content">
+                <div>
+                    <img src="https://media-exp1.licdn.com/dms/image/C4D1BAQGAO9zvVhyAqQ/company-background_10000/0/1539806088038?e=2159024400&v=beta&t=5vq2-UBGsfB5RPQaOHqFEHtmdq47wFGYL27qOMblKwc" />
+                </div>
+                <div>Avatar + description</div>
+                <div>My posts</div>
+                <div>My wall</div>
+            </main>
+    )
+}
+export default Profile;
