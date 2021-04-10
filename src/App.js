740fb8cf7e937cd390a79337ab15785198bfad49
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-04-10 13:51:20 +0300

    11l

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 "use strict";
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
+
 
 const App = () => {
   return (
-    <div className="App">
-      Simple HTML   
-      <Header />
-      <Technologies /> 
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfoCuO-KYuaQsPqEYUx8zYvMcswYLYqZZGyd_Usx3_eQyomUM7XTyBmFGfcw56Hu_nfEA&usqp=CAU"/>
+      </header>  
+      <nav className="nav">
+        <ul>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Messages</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Music</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </nav>
+      <main className="content">
+        <div>
+          <img src="https://media-exp1.licdn.com/dms/image/C4D1BAQGAO9zvVhyAqQ/company-background_10000/0/1539806088038?e=2159024400&v=beta&t=5vq2-UBGsfB5RPQaOHqFEHtmdq47wFGYL27qOMblKwc"/>
+        </div>
+        <div>Avatar + description</div>
+        <div>My posts</div>
+        <div>My wall</div>
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Technologies from './Technologies';
+
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Header from './Header';
+
 
 ReactDOM.render(
   <React.StrictMode>
