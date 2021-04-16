c59cff3ebbf314204dcc7f65f23d8eb76f8f1475
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-04-16 22:24:37 +0300

    14l

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import s from './Header.module.css';
 const Header = () => {
-    return (<header className="header">
+    return (<header className={s.header}>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfoCuO-KYuaQsPqEYUx8zYvMcswYLYqZZGyd_Usx3_eQyomUM7XTyBmFGfcw56Hu_nfEA&usqp=CAU" />
     </header>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import s from './Profile.module.css';
 const Profile = () => {
     return (
-            <main className="content">
+            <main className={s.content}>
                 <div>
                     <img src="https://media-exp1.licdn.com/dms/image/C4D1BAQGAO9zvVhyAqQ/company-background_10000/0/1539806088038?e=2159024400&v=beta&t=5vq2-UBGsfB5RPQaOHqFEHtmdq47wFGYL27qOMblKwc" />
                 </div>
-                <div>Avatar + description</div>
-                <div>My posts</div>
+                <div className={s.description}>Avatar + description</div>
+                <div className={s.posts}>My posts</div>
                 <div>My wall</div>
             </main>
     )
