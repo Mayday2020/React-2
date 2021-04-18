8bcda299b641a2384ab62cc69c3473a70031db93
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-04-18 23:58:50 +0300

    15l

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts';
+
 const Profile = () => {
     return (
             <main className={s.content}>
@@ -7,8 +9,7 @@ const Profile = () => {
                     <img src="https://media-exp1.licdn.com/dms/image/C4D1BAQGAO9zvVhyAqQ/company-background_10000/0/1539806088038?e=2159024400&v=beta&t=5vq2-UBGsfB5RPQaOHqFEHtmdq47wFGYL27qOMblKwc" />
                 </div>
                 <div className={s.description}>Avatar + description</div>
-                <div className={s.posts}>My posts</div>
-                <div>My wall</div>
+                <MyPosts />
             </main>
     )
 }
